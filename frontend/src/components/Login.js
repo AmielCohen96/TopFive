f2bd6a9310b790fed0c7bcca86aad3e8a2affeee
AmielCohen96 <117925491+AmielCohen96@users.noreply.github.com>
2024-08-13 14:49:41 +0300

    Top Five

---
+++ frontend/src/components/Login.js
@@ -0,0 +1,88 @@
+import React, { useState } from 'react';
+import './LoginComponent.css';
+import {Link} from "react-router-dom";
+import axios from "axios";
+
+
+
+const Login = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+
+    const handleUsernameChange = (event) => {
+        setUsername(event.target.value);
+    };
+
+    const handlePasswordChange = (event) => {
+        setPassword(event.target.value);
+    };
+
+    const handleSubmit = async (event) => {
+        console.log('Username:', username);
+        console.log('Password:', password);
+
+        event.preventDefault();
+        try {
+            const response = await axios.post('http://localhost:9124/login', null,{params: {
+                    username,
+                    password
+                }});
+            console.log('Login response:', response.data);
+            if (response.data.success) {
+                console.log("good")
+                // window.location.href = '/stream-page';
+                window.location.href = `/stream-page?username=${encodeURIComponent(username)}&password=${encodeURIComponent(password)}`;
+                // Redirect to dashboard
+            } else {
+                console.log("bad")
+                setError('Invalid username or password');
+            }
+        } catch (error) {
+            console.error('Login failed:', error);
+            setError('Login failed. Please try again.');
+        }
+
+    };
+
+    const startWithoutSignIn = () => {
+        console.log('without')
+        window.location.href = '/without-login-stream';
+    }
+
+    const handleSignUp = () => {
+        console.log('Redirecting to sign up page...');
+        window.location.href = '/signup';
+    };
+
+    return (
+        <div className="login-container">
+            <h2>Login</h2>
+            <div className="input-container">
+                <span>Username </span>
+                <input
+                    type="text"
+                    placeholder="Username"
+                    value={username}
+                    onChange={handleUsernameChange}
+                />
+            </div>
+            <div className="input-container">
+                <span>Password </span>
+                <input
+                    type="password"
+                    placeholder="Password"
+                    value={password}
+                    onChange={handlePasswordChange}
+                />
+            </div>
+            <div className="button-container">
+                <button onClick={handleSubmit}>Sign In</button>
+                <button onClick={handleSignUp}>Sign Up</button>
+                <button onClick={startWithoutSignIn}>Start without Sign In</button>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
---
+++ frontend/src/components/signUp.js
@@ -0,0 +1,115 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom'; // Import Link from React Router
+import './SignupComponent.css';
+
+const signUp = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [email, setEmail] = useState('');
+    const [error, setError] = useState('');
+    const [success, setSuccess] = useState(false); // State for success message
+    const [signingUp, setSigningUp] = useState(false); // State for controlling sign-up process
+
+    const handleUsernameChange = (event) => {
+        setUsername(event.target.value);
+    };
+
+    const handlePasswordChange = (event) => {
+        setPassword(event.target.value);
+    };
+
+    const handleEmailChange = (event) => {
+        setEmail(event.target.value);
+    };
+
+    const validateForm = () => {
+        if (!username) {
+            setError('Please enter a username');
+            return false;
+        }
+        if (!password || password.length < 6 || !/\d/.test(password) || !/[a-zA-Z]/.test(password)) {
+            setError('Password must be at least 6 characters long and include both letters and numbers');
+            return false;
+        }
+        if (!email || !/\S+@\S+\.\S+/.test(email)) {
+            setError('Please enter a valid email address');
+            return false;
+        }
+        return true;
+    };
+
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        if (validateForm()) {
+            setError('');
+            setSigningUp(true); // Start sign-up process
+            try {
+                const response = await axios.post('http://localhost:9124/add-user', null, {
+                    params: {
+                        username,
+                        password,
+                        email
+                    }
+                });
+                console.log('Signup successful:', response.data);
+                setSuccess(true); // Set success state to true
+            } catch (error) {
+                console.error('Signup failed:', error);
+                setError('Signup failed. Please try again.');
+            } finally {
+                setSigningUp(false); // Reset signing up state
+            }
+        }
+    };
+
+    return (
+        <div className="signup-container">
+            {success ? ( // Show success message if sign-up was successful
+                <div className="success-message">
+                    <p>Signup successful! You can now proceed to login.</p>
+                    <button><Link to="/">Back to Login</Link></button>
+                </div>
+            ) : (
+                <React.Fragment>
+                    <h2>Sign Up</h2>
+                    <form onSubmit={handleSubmit}>
+                        <div className="input-container">
+                            <span>Username:</span>
+                            <input
+                                type="text"
+                                placeholder="Username"
+                                value={username}
+                                onChange={handleUsernameChange}
+                            />
+                        </div>
+                        <div className="input-container">
+                            <span>Password:</span>
+                            <input
+                                type="password"
+                                placeholder="Password"
+                                value={password}
+                                onChange={handlePasswordChange}
+                            />
+                        </div>
+                        <div className="input-container">
+                            <span>Email:</span>
+                            <input
+                                type="email"
+                                placeholder="Email"
+                                value={email}
+                                onChange={handleEmailChange}
+                            />
+                        </div>
+                        {error && <p className="error-message">{error}</p>}
+                        <button type="submit" disabled={signingUp}>{signingUp ? 'Signing up...' : 'Sign Up'}</button>
+                        <button><Link to="/">Back to Login</Link></button>
+                    </form>
+                </React.Fragment>
+            )}
+        </div>
+    );
+};
+
+export default signUp;
