124302d8dcbecb5cc309fa58ef9cd3b12062e0d9
AmielCohen96 <117925491+AmielCohen96@users.noreply.github.com>
2024-08-21 17:58:28 +0300

    Top Five - 15:07 21/8/24

--- topFive/admin.py
+++ topFive/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
-from .models import Coach, Player, Team, League
+from .models import Coach, Player, Team, League, CustomUser
+from django.contrib.auth.admin import UserAdmin
+
 
 @admin.register(Coach)
 class CoachAdmin(admin.ModelAdmin):
     list_display = ('name', 'defense', 'offense')
+
 
 @admin.register(Player)
 class PlayerAdmin(admin.ModelAdmin):
@@ -12,12 +15,35 @@ class PlayerAdmin(admin.ModelAdmin):
         'shooting3', 'shooting2', 'jumping', 'defense', 'rating'
     )
 
+
 @admin.register(Team)
 class TeamAdmin(admin.ModelAdmin):
     list_display = ('name', 'manager', 'coach', 'budget', 'league', 'points', 'position', 'average_rating', 'arena')
     filter_horizontal = ('players',)
 
+
 @admin.register(League)
 class LeagueAdmin(admin.ModelAdmin):
     list_display = ('name', 'level')
     filter_horizontal = ('teams',)
+
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ('email', 'username', 'is_staff', 'is_active',)
+    list_filter = ('email', 'username', 'is_staff', 'is_active',)
+    fieldsets = (
+        (None, {'fields': ('email', 'username', 'password')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active', 'groups', 'user_permissions')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'username', 'password1', 'password2', 'is_staff', 'is_active')}
+         ),
+    )
+    search_fields = ('email', 'username',)
+    ordering = ('email',)
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
